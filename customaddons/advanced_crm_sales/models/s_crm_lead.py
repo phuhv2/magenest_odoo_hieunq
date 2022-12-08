7ff1a75e93059ed940ade8f447d2272407f83369
hieunqdev <hieunqdev@gmail.com>
2022-12-08 14:09:39 +0700

    ODOO_RM_INTERN-088: fix bug recordset id edit minimum revenue

--- customaddons/advanced_crm_sales/models/s_crm_lead.py
+++ customaddons/advanced_crm_sales/models/s_crm_lead.py
@@ -21,11 +21,12 @@ class SCrmLead(models.Model):
 
     # Check if count of sales order > 0, then minimum_revenue is readonly
     def _compute_edit_minimum_revenue(self):
-        if self.id:
-            count_sale_order = self.env['sale.order'].search_count([('opportunity_id', '=', self.id)])
-            self.edit_minimum_revenue = False
-            if count_sale_order > 0:
-                self.edit_minimum_revenue = True
+        for rec in self:
+            if rec.id:
+                count_sale_order = self.env['sale.order'].search_count([('opportunity_id', '=', self.id)])
+                rec.edit_minimum_revenue = False
+                if count_sale_order > 0:
+                    rec.edit_minimum_revenue = True
 
     # Calculate real_revenue = amount_total corresponding to the opportunity
     def _compute_real_revenue(self):
