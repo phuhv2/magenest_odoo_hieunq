2a25d1ab1d9d527cc77552ce9ee827d1fce8a749
hieunqdev <hieunqdev@gmail.com>
2022-12-15 16:19:42 +0700

    ODOO_RM_INTERN-088: Fix bug button confirm of accountant

--- customaddons/advanced_purchase/models/s_purchase_order.py
+++ customaddons/advanced_purchase/models/s_purchase_order.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import json
 
 
 class SPurchaseOrder(models.Model):
@@ -10,12 +11,16 @@ class SPurchaseOrder(models.Model):
 
     # Override check the creator has exceeded the limit
     def button_confirm(self):
-        current_user_id = self.env.uid
-        employee_line = self.env['employee.order.limit'].search([('name', '=', current_user_id)], limit=1)
+        res_partner_user_id = self.env['res.partner'].search([('write_uid', '=', self.env.uid)])
+        current_user_id = res_partner_user_id.mapped('id')
+        employee_line = self.env['employee.order.limit'].search([], limit=1)
         employee = employee_line.mapped('order_limit')
 
         # get users of accountant
-        res_groups = self.env['res.groups'].sudo().search([('id', '=', 52)])
+        id_group_accountant = self.env.cr.execute(
+            "SELECT id FROM res_groups WHERE name::text LIKE '%Accountant%';")
+        id_group_accountant_result = self.env.cr.fetchall()
+        res_groups = self.env['res.groups'].sudo().search([('id', 'in', id_group_accountant_result)])
         res_groups_id = res_groups.mapped('users')
         res_groups_users = res_groups_id.mapped('id')
 
@@ -24,15 +29,13 @@ class SPurchaseOrder(models.Model):
         res_users_id = res_users.mapped('partner_id')
         res_users_partner_id = res_users_id.mapped('id')
 
-        # get email of accountant
+        # get id of accountant
         res_partner = self.env['res.partner'].sudo().search([('id', 'in', res_users_partner_id)])
         accountant_id = res_partner.mapped('id')
 
         for rec in self:
             if rec.amount_total:
-                if current_user_id in accountant_id:
-                    return super(SPurchaseOrder, self).button_confirm()
-                elif rec.amount_total < employee[0] and rec.check_send == False:
+                if (rec.amount_total < employee[0]) or (current_user_id[0] in accountant_id):
                     return super(SPurchaseOrder, self).button_confirm()
                 else:
                     raise ValidationError('The total request exceeds the limit. Please send it to the accountant.')
@@ -41,7 +44,3 @@ class SPurchaseOrder(models.Model):
         self.message_post(body=f'{self.create_uid.name} -> Confirmation request sent.')
         for rec in self:
             rec.check_send = True
-
-    # Override button confirm for accountant
-    # def btn_confirm_order(self):
-    #     return super(SPurchaseOrder, self).button_confirm()
