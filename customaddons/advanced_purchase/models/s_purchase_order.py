56929952caf785810e9f16330e60297f2a6c2d62
hieunqdev <hieunqdev@gmail.com>
2022-12-16 10:58:55 +0700

    ODOO_RM_INTERN-088: Change query list id of accountant

--- customaddons/advanced_purchase/models/s_purchase_order.py
+++ customaddons/advanced_purchase/models/s_purchase_order.py
@@ -11,12 +11,9 @@ class SPurchaseOrder(models.Model):
 
     # Override check the creator has exceeded the limit
     def button_confirm(self):
-        res_partner_user_id = self.env['res.partner'].search([('write_uid', '=', self.env.uid)])
-        current_user_id = res_partner_user_id.mapped('id')
-        employee_line = self.env['employee.order.limit'].search([], limit=1)
-        employee = employee_line.mapped('order_limit')
+        current_uid = self.env.uid
 
-        # get users of accountant
+        # get list id of accountant
         id_group_accountant = self.env.cr.execute(
             "SELECT id FROM res_groups WHERE name::text LIKE '%Accountant%';")
         id_group_accountant_result = self.env.cr.fetchall()
@@ -24,18 +21,13 @@ class SPurchaseOrder(models.Model):
         res_groups_id = res_groups.mapped('users')
         res_groups_users = res_groups_id.mapped('id')
 
-        # get partner_id of accountant
-        res_users = self.env['res.users'].sudo().search([('id', 'in', res_groups_users)])
-        res_users_id = res_users.mapped('partner_id')
-        res_users_partner_id = res_users_id.mapped('id')
-
-        # get id of accountant
-        res_partner = self.env['res.partner'].sudo().search([('id', 'in', res_users_partner_id)])
-        accountant_id = res_partner.mapped('id')
+        # get order limit of current employee
+        employee_line = self.env['employee.order.limit'].search([], limit=1)
+        employee = employee_line.mapped('order_limit')
 
         for rec in self:
             if rec.amount_total:
-                if (rec.amount_total < employee[0]) or (current_user_id[0] in accountant_id):
+                if (rec.amount_total < employee[0]) or (current_uid in res_groups_users):
                     return super(SPurchaseOrder, self).button_confirm()
                 else:
                     raise ValidationError('The total request exceeds the limit. Please send it to the accountant.')
